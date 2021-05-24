e4345207f9f4847a000b8ad96e5c5225d82f07cf
Dylan Brain <dylanbrain@googlemail.com>
2021-05-24 21:13:57 +0900

    initial push

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Header = (props) => {
+    return <div>
+      this is a header
+  </div>
+};
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Main = (props) => {
+  return <div>this is the main</div>;
+};
+
+export default Main;
