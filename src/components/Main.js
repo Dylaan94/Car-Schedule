fc92ddbf7e674bce6f75a4718ecba98e70cf2ab7
Dylan Brain <dylanbrain@googlemail.com>
2021-05-24 21:58:15 +0900

    added names

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import MaterialTable from "material-table";
 
-const Main = (props) => {
-  return <div>this is the main</div>;
-};
+class Main extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <MaterialTable
+          columns={[
+            { title: "Day", field: "Monday" },
+            { title: "Marie" },
+            { title: "Imelda" },
+            { title: "Carina" },
+            { title: "Jessica" },
+            { title: "Michael" },
+            { title: "Haniel" },
+            { title: "Erik" },
+            { title: "Dylan" },
+          ]}
+          title="Bronomichi Car Schedule"
+        ></MaterialTable>
+        this is the main bit yo
+      </div>
+    );
+  }
+}
 
 export default Main;
