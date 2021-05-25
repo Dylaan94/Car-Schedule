f02378c4799e8ff914e1a25866661ac496e544eb
Dylan Brain <dylanbrain@googlemail.com>
2021-05-25 23:01:44 +0900

    experimenting with react-table

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,32 +1,190 @@
 import React, { Component } from "react";
-import MaterialTable from "material-table";
+import { useTable } from "react-table";
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-  render() {
-    return (
-      <div>
-        <MaterialTable
-          columns={[
-            { title: "Day", field: "Monday" },
-            { title: "Marie" },
-            { title: "Imelda" },
-            { title: "Carina" },
-            { title: "Jessica" },
-            { title: "Michael" },
-            { title: "Haniel" },
-            { title: "Erik" },
-            { title: "Dylan" },
-          ]}
-          title="Bronomichi Car Schedule"
-        ></MaterialTable>
-        this is the main bit yo
-      </div>
-    );
-  }
-}
+const Main = () => {
+  const data = React.useMemo(() => [
+    {
+      col1: "Days",
+      col2: "Marie",
+      col3: "Imelda",
+      col4: "Carina",
+      col5: "Jessica",
+      col6: "Mike",
+      col7: "Haniel",
+      col8: "Erik",
+      col9: "Dylan",
+    },
+    {
+      col1: "Mon",
+      col2: "",
+      col3: "",
+      col4: "",
+      col5: "",
+      col6: "",
+      col7: "",
+      col8: "",
+      col9: "",
+    },
+    {
+      col1: "Tue",
+      col2: "",
+      col3: "",
+      col4: "",
+      col5: "",
+      col6: "",
+      col7: "",
+      col8: "",
+      col9: "",
+    },
+    {
+      col1: "Wed",
+      col2: "",
+      col3: "",
+      col4: "",
+      col5: "",
+      col6: "",
+      col7: "",
+      col8: "",
+      col9: "",
+    },
+    {
+      col1: "Thur",
+      col2: "",
+      col3: "",
+      col4: "",
+      col5: "",
+      col6: "",
+      col7: "",
+      col8: "",
+      col9: "",
+    },
+    {
+      col1: "Fri",
+      col2: "",
+      col3: "",
+      col4: "",
+      col5: "",
+      col6: "",
+      col7: "",
+      col8: "",
+      col9: "",
+    },
+    {
+      col1: "Sat",
+      col2: "",
+      col3: "",
+      col4: "",
+      col5: "",
+      col6: "",
+      col7: "",
+      col8: "",
+      col9: "",
+    },
+    {
+      col1: "Sun",
+      col2: "",
+      col3: "",
+      col4: "",
+      col5: "",
+      col6: "",
+      col7: "",
+      col8: "",
+      col9: "",
+    },
+  ]);
+
+  const columns = React.useMemo(() => [
+    {
+      Header: "Column1",
+      accessor: "col1",
+    },
+    {
+      Header: "Colum2",
+      accessor: "col2",
+    },
+    {
+      Header: "Colum3",
+      accessor: "col3",
+    },
+    {
+      Header: "Colum4",
+      accessor: "col4",
+    },
+    {
+      Header: "Colum5",
+      accessor: "col5",
+    },
+    {
+      Header: "Colum6",
+      accessor: "col6",
+    },
+    {
+      Header: "Colum7",
+      accessor: "col7",
+    },
+    {
+      Header: "Colum8",
+      accessor: "col8",
+    },
+    {
+      Header: "Column9",
+      accessor: "col9",
+    },
+  ]);
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data });
+  return (
+    <table {...getTableProps()} style={{ border: "solid 1px blue" }}>
+      <thead>
+        {headerGroups.map((headerGroup) => (
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {headerGroup.headers.map((column) => (
+              <th
+                {...column.getHeaderProps()}
+                style={{
+                  borderBottom: "solid 3px red",
+                  background: "aliceblue",
+                  color: "black",
+                  fontWeight: "bold",
+                }}
+              >
+                {column.render("Header")}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody {...getTableBodyProps()}>
+        {rows.map((row) => {
+          prepareRow(row);
+          return (
+            <tr {...row.getRowProps()}>
+              {row.cells.map((cell) => {
+                return (
+                  <td
+                    {...cell.getCellProps()}
+                    style={{
+                      padding: "10px",
+                      border: "solid 1px gray",
+                      background: "papayawhip",
+                    }}
+                  >
+                    {cell.render("Cell")}
+                  </td>
+                );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
+};
 
 export default Main;
---
+++ src/components/data.js
@@ -0,0 +1,66 @@
+// const columnData = React.useMemo(() => {
+//   [
+//     {
+//       col1: "Days",
+//       col2: "Mon",
+//       col3: "Tue",
+//       col4: "Wed",
+//       col5: "Thurs",
+//       col6: "Fri",
+//       col7: "Sat",
+//       col8: "Sun",
+//     },
+//     {
+//       col1: "Marie",
+//       col2: "",
+//       col3: "",
+//       col4: "",
+//       col5: "",
+//       col6: "",
+//       col7: "",
+//       col8: "",
+//     },
+//   ];
+// });
+
+// const columns = React.useMemo(() => {
+//   [
+//     {
+//       Header: "Column1",
+//       accessor: "col1",
+//     },
+//     {
+//       Header: "Colum2",
+//       accessor: "col2",
+//     },
+//     {
+//       Header: "Colum3",
+//       accessor: "col3",
+//     },
+//     {
+//       Header: "Colum4",
+//       accessor: "col4",
+//     },
+//     {
+//       Header: "Colum5",
+//       accessor: "col5",
+//     },
+//     {
+//       Header: "Colum6",
+//       accessor: "col6",
+//     },
+//     {
+//       Header: "Colum7",
+//       accessor: "col7",
+//     },
+//     {
+//       Header: "Colum8",
+//       accessor: "col8",
+//     },
+//   ];
+// });
+
+// const rows = React.useMemo(() => {
+//   data here
+// });
+
