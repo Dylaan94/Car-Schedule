09e5b30560c7f69019e5c4c5dddfa4f7a3c75e18
Dylan Brain <dylanbrain@googlemail.com>
2021-08-11 14:20:10 +0900

    Updates

--- src/components/Sidebar.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import SidebarStyles from "./styles/SidebarStyles"
-
-const Sidebar = (props) => {
-  return <SidebarStyles.Sidebar>this is a sidebar</SidebarStyles.Sidebar>;
-};
-
-export default Sidebar;
--- src/components/styles/SidebarStyles.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const Sidebar = styled.div``;
-
-const SidebarStyles = {
-  Sidebar: Sidebar,
-};
-
-export default SidebarStyles;
